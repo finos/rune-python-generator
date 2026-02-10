530d169729fb8194d6a529d31122bdd8437bc100
dschwartznyc <daniel.schwartz@ftadvisory.co>
2026-02-10 15:57:05 -0500

    chore: restore test_entity_reuse.py to fail.  remove extraneous tests

--- fail_test.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-from typing import Annotated
-class B:
-    print('Defining B')
-    x: Annotated['A', A.foo()]
-class A:
-    @staticmethod
-    def foo(): return 1
-print('SUCCESS')
--- test/python_unit_tests/features/model_structure/test_entity_reuse.py
+++ test/python_unit_tests/features/model_structure/test_entity_reuse.py
@@ -1,4 +1,6 @@
-"""Test entity reuse."""
+import pytest
+
+from pydantic import ValidationError
 
 from rosetta_dsl.test.model.reuse_type.BaseEntity import BaseEntity
 from rosetta_dsl.test.model.reuse_type.BarNoRef import BarNoRef
@@ -9,7 +11,8 @@ def test_entity_reuse():
     """Test entity reuse."""
     base_entity = BaseEntity(number=1)
     BarRef(bar=base_entity)
-    BarNoRef(bar=base_entity)
+    with pytest.raises(ValidationError):
+        BarNoRef(bar=base_entity)
 
 
 if __name__ == "__main__":
